d1c0377281f3a7dec7ccfe7ba9716641480eb4a0
Director-png <anuj05758@gmail.com>
2026-02-27 10:20:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,11 +160,21 @@ if 'logged_in' not in st.session_state: st.session_state.logged_in = False
 if 'last_topic' not in st.session_state: st.session_state.last_topic = "General AI Intelligence"
 if 'user_name' not in st.session_state: st.session_state.user_name = "Guest"
 if 'user_role' not in st.session_state: st.session_state.user_role = "user"
-if 'is_paid' not in st.session_state:
-    st.session_state.is_paid = True # Set to True for your personal testing
+# --- IDENTITY INITIALIZATION ---
 if 'is_admin' not in st.session_state:
-    st.session_state.is_admin = True
+    st.session_state.is_admin = False # Default to False for safety
 
+if 'is_paid' not in st.session_state:
+    st.session_state.is_paid = False # Default to False
+
+# --- FOUNDER BYPASS (ONLY FOR YOUR TEST SESSIONS) ---
+# When you want to see the BASIC version to check the UI, set this to False.
+# When you want to work on the OPERATIVE version, set this to True.
+FOUNDER_MODE = True 
+
+if FOUNDER_MODE:
+    st.session_state.is_admin = True
+    st.session_state.is_paid = True
 # --- 🛰️ DATA INFRASTRUCTURE ---
 MARKET_PULSE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTuN3zcXZqn9RMnPs7vNEa7vI9xr1Y2VVVlZLUcEwUVqsVqtLMadz1L_Ap4XK_WPA1nnFdpqGr8B_uS/pub?output=csv"
 USER_DB_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT8sFup141r9k9If9fu6ewnpWPkTthF-rMKSMSn7l26PqoY3Yb659FIDXcU3UIU9mo5d2VlR2Z8gHes/pub?gid=2093671902&single=true&output=csv&t=" + str(time.time())
@@ -2554,6 +2564,7 @@ with f_col3:
     st.caption("📍 Udham Singh Nagar, Uttarakhand, India")
 
 st.markdown("<p style='text-align: center; font-size: 10px; color: gray;'>Transaction Security by Razorpay | © 2026 VOID OS</p>", unsafe_allow_html=True)
+
 
 
 
